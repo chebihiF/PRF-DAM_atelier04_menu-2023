72eb71bf3ff00fadb61b398e6fea3ad7c1d18cbc
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-05-09 11:49:38 +0100

    final version

--- app/src/main/java/org/m2i/atelier04_menu/FavoriteActivity.java
+++ app/src/main/java/org/m2i/atelier04_menu/FavoriteActivity.java
@@ -1,14 +1,52 @@
 package org.m2i.atelier04_menu;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class FavoriteActivity extends AppCompatActivity {
+
+    TextView msg_txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorite);
+
+        msg_txt = findViewById(R.id.msg_txt);
+        registerForContextMenu(msg_txt);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.context_share){
+            AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+            alertBuilder.setTitle("Share option");
+            alertBuilder.setMessage("do you want to share this content ? ");
+            alertBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Log.i("testtest","Click on action OK");
+                }
+            });
+            alertBuilder.show();
+        }
+        return super.onContextItemSelected(item);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_context, menu);
     }
 }
